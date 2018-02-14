a6ade9fcd98207ae09194e76aedfcc6aad0f8863
Guillem Ramírez Miranda <guillem.ramirez.miranda@est.fib.upc.edu>
2018-02-14 16:20:08 +0100

    Now you can load and save progress!

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Neural network python class from scratch using python.
 ## Features
 + Variable number of layers, we can choose 1 or 2 hidden layers
-+ Possible to specify size of layers 
++ Possible to specify size of layers
 + You can specify activation function using lambda's
++ save current state of neural network with save("path")
++ load a state with specifing path in the constructor
--- nnfromscratch.py
+++ nnfromscratch.py
@@ -1,8 +1,7 @@
-%matplotlib inline
-import matplotlib
+# %matplotlib inline
+import os
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
 np.random.seed(2) #for the sake of debuging we set the random seed
 
 
@@ -15,29 +14,57 @@ class neuralNetwork():
                  nHidden_layers=1,
                  output_neurons=2,
                  activation_function=lambda x: 1 / (1+np.exp(-x)),
-                 derivate_function=lambda x: x*(1-x)):
-        if nHidden_layers != 1 and nHidden_layers != 2:
-            print "Number of hidden layers possible is 1 or 2!"
-            sys.exit()
+                 derivate_function=lambda x: x*(1-x),
+                 path = None):
+        if path == None:
+            if nHidden_layers != 1 and nHidden_layers != 2:
+                print "Number of hidden layers possible is 1 or 2!"
+                sys.exit(2)
 
-        self.nHidden = nHidden_layers
+            self.nHidden = nHidden_layers
 
-        if self.nHidden == 1:
-            self.wIH = 2*np.random.rand(hidden_neurons1,input_neurons)  -1
-            self.wHO = 2*np.random.rand(output_neurons,hidden_neurons1) -1
-            self.bIH = 2*np.random.rand(hidden_neurons1,1) -1
-            self.bHO = 2*np.random.rand(output_neurons,1)  -1
-        if self.nHidden == 2:
-            self.wIH = 2*np.random.rand(hidden_neurons1, input_neurons)   -1
-            self.wHH = 2*np.random.rand(hidden_neurons2, hidden_neurons1) -1
-            self.wHO = 2*np.random.rand(output_neurons, hidden_neurons2)  -1
-            self.bIH = 2*np.random.rand(hidden_neurons1, 1) -1
-            self.bHH = 2*np.random.rand(hidden_neurons2, 1) -1
-            self.bHO = 2*np.random.rand(output_neurons, 1)  -1
+            if self.nHidden == 1:
+                self.wIH = 2*np.random.rand(hidden_neurons1,input_neurons)  -1
+                self.wHO = 2*np.random.rand(output_neurons,hidden_neurons1) -1
+                self.bIH = 2*np.random.rand(hidden_neurons1,1) -1
+                self.bHO = 2*np.random.rand(output_neurons,1)  -1
+            if self.nHidden == 2:
+                self.wIH = 2*np.random.rand(hidden_neurons1, input_neurons)   -1
+                self.wHH = 2*np.random.rand(hidden_neurons2, hidden_neurons1) -1
+                self.wHO = 2*np.random.rand(output_neurons, hidden_neurons2)  -1
+                self.bIH = 2*np.random.rand(hidden_neurons1, 1) -1
+                self.bHH = 2*np.random.rand(hidden_neurons2, 1) -1
+                self.bHO = 2*np.random.rand(output_neurons, 1)  -1
 
-        #and we set activation + derivate functions
+
+        else:
+            if not os.path.exists(path):
+                print "No data to load!!"
+                sys.exit(2)
+            self.wIH = np.load(path+"/wIH.npy")
+            self.bIH = np.load(path+"/bIH.npy")
+            if os.path.isfile(path+"/wHH.npy") and os.path.isfile(path+"/bHH.npy"):
+                self.wHH = np.load(path+"/wHH.npy")
+                self.bHH = np.load(path+"/bHH.npy")
+                self.nHidden = 2
+            else:
+                self.nHidden = 1
+            self.wHO = np.load(path+"/wHO.npy")
+            self.bHO = np.load(path+"/bHO.npy")
+                    #and we set activation + derivate functions
         self.activation_function = activation_function
         self.derivate_function = derivate_function
+    #save de matrix's
+    def save(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+        np.save(path+"/wIH",self.wIH)
+        np.save(path+"/bIH",self.bIH)
+        if self.nHidden == 2:
+            np.save(path+"/wHH", self.wHH)
+            np.save(path+"/bHH", self.bHH)
+        np.save(path+"/wHO", self.wHO)
+        np.save(path+"/bHO", self.bHO)
 
     #validation, tell neural network to make a prediction
     def validate(self, inputs):
@@ -77,9 +104,9 @@ class neuralNetwork():
             if i%logStep == 0 and i >100:
                 lossArray.append(totalErr/i)
                 stepArray.append(i)
-        plt.plot(stepArray,lossArray)
-        plt.ylabel('loss evolution')
-        plt.show() #once we finished we show the plot of the evolution of the error
+        #plt.plot(stepArray,lossArray)
+        #plt.ylabel('loss evolution')   ####PLOT THE ERROR
+        #plt.show() #once we finished we show the plot of the evolution of the error
 
     def step_train(self, inputs, outputs, learning_rate,i):
         #transpose the input, make it a vertical column
@@ -114,7 +141,6 @@ class neuralNetwork():
             self.bHO = np.add(self.bHO, deltaBHO)
             self.bIH = np.add(self.bIH, deltaBIH)
         else:
-            ### same but with 2 layers
             h1  = self.apply_function(np.add(np.dot(self.wHH, h0), self.bHH), self.activation_function)
             out = self.apply_function(np.add(np.dot(self.wHO, h1), self.bHO), self.activation_function)
 
@@ -155,9 +181,12 @@ class neuralNetwork():
 
         return x
 
-###### for generate dataset ##############################
-#we take array of 4 bits [x1, x0, y1, y0]
-#and we want the sum in a 3 bit array
+
+# We will use the neural network class for adding to numbers
+# Our input will be [x2, x1, y2, y1] and the ouput will be x+y
+#where xn represents a binary bit of x
+#############for generating dataset##########################
+#generates every combination of binary numbers of 4 bits
 def backtracking(i, marcas, solucions):
     if i == 4:
         solucions.append(marcas)
@@ -184,14 +213,19 @@ def run():
     drelu = lambda x: 1 if x > 0 else 0
     tanh = lambda x: (2/(1+np.exp(-2*x))) -1
     dtanh = lambda x: 1 - x**2
+    #create a new neural network
     nn = neuralNetwork(
                  input_neurons=4,
                  hidden_neurons1=100,
                  hidden_neurons2=50,
-                 nHidden_layers=2, #not very usefull for this problem, it just reduces eficiency
+                 nHidden_layers=2,
                  output_neurons=3,
                  activation_function=tanh,
                  derivate_function=dtanh)
+
+   ## nn = neuralNetwork(path="./data", activation_function=tanh, derivate_function=dtanh) #for loading from a data folder
+
+    #generate train set
     inputs = []
     backtracking(0, [0,0,0,0], inputs)
     #generate solutions
@@ -201,10 +235,11 @@ def run():
         outputs.append(x)
 
     #train neural network
-    nn.train(inputs, outputs, 10000)
+    nn.train(inputs, outputs, 40000)
     #validate simple value
     for i in inputs:
         print i
         print nn.validate(i)
+    ##nn.save("./data") ## save the weights of your current nn into ./data folder, you can specify the folder you want, it will be created
 if __name__ == "__main__":
     run()
