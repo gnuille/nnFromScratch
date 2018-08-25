3a52666797f323fad86b0f5759033afec1173033
teleportex <19.guillemramirez19@gmail.com>
2018-08-25 12:28:20 +0200

    added error calculation for each weight

--- C/neuralNetwork.c
+++ C/neuralNetwork.c
@@ -6,9 +6,9 @@
 void loadNn(struct nn* neural,int n_layers, int sizes[], int input, int output, actFunc act){
   neural->n_layers = n_layers;
   neural->layer_sizes = sizes;
-  neural->weights = malloc(sizeof(gsl_matrix*)*n_layers +1);
-  neural->biases  = malloc(sizeof(gsl_matrix*)*n_layers +1);
-  neural->results = malloc(sizeof(gsl_matrix*)*n_layers +1);
+  neural->weights = malloc(sizeof(gsl_matrix*)*(n_layers +1));
+  neural->biases  = malloc(sizeof(gsl_matrix*)*(n_layers +1));
+  neural->results = malloc(sizeof(gsl_matrix*)*(n_layers +1));
 
   neural->weights[0] = gsl_matrix_calloc(neural->layer_sizes[0], input);
   neural->biases[0]  = gsl_matrix_calloc(neural->layer_sizes[0], 1    );
@@ -79,16 +79,18 @@ void predictNn(struct nn* neural, double input[]){
 
   printf("Result:\n");
   printMatrix(inp);
-
+  gsl_matrix_free(inp);
 }
 
 void stepTrain(struct nn* neural, double* input, double* output){
+  //transform input and output vectors to matrices for easly working
   gsl_matrix *inp = gsl_matrix_alloc(neural->weights[0]->size2, 1);
   gsl_matrix *out = gsl_matrix_alloc(neural->weights[neural->n_layers]->size1, 1);
 
   fromArrayToColumn(inp, input);
   fromArrayToColumn(out, output);
 
+  //fastforward like prediction and store temporary results for calculating errors
   int i;
   for(i = 0; i <=neural->n_layers; i++){
     multiplyMatrix(neural->weights[i], inp, inp);
@@ -96,5 +98,20 @@ void stepTrain(struct nn* neural, double* input, double* output){
     if(i == neural->n_layers) applyFunMatrix(inp, neural->act);
     gsl_matrix_memcpy(neural->results[i],inp);
   }
-  printMatrixArray(neural->results);
+
+  //array of matrices for errors of each layer
+  gsl_matrix** errors = malloc(sizeof(gsl_matrix*)*(neural->n_layers+1));
+  for(i = 0; i<=neural->n_layers; i++){
+    errors[i] = gsl_matrix_alloc(neural->biases[i]->size1,1);
+  }
+  //calculate the errors
+  gsl_matrix_memcpy(errors[neural->n_layers],out);
+  gsl_matrix_sub(errors[neural->n_layers], inp);
+  for (i = neural->n_layers; i > 0; i--){
+    gsl_matrix* weightT = gsl_matrix_alloc(neural->weights[i]->size2,neural->weights[i]->size1);
+    gsl_matrix_transpose_memcpy(weightT,neural->weights[i]);
+    multiplyMatrix(weightT, errors[i], errors[i-1]);
+    gsl_matrix_free(weightT);
+  }
+  printMatrixArray(errors, neural->n_layers+1);
 }
